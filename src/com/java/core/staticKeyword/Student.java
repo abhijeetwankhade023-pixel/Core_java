ec4bd95184063bbe3f733870a6eb52a3338565a1
DELL <DELL@DESKTOP-7HSFUTK>
2026-05-01 15:46:59 +0530

    Core Java

---
+++ src/com/java/core/staticKeyword/Student.java
@@ -0,0 +1,14 @@
+package com.java.core.staticKeyword;
+
+public class Student {
+	
+	public static String name = "Ayush";
+	
+	public String city = "Pune";
+	
+	public static void printAge() {
+		
+		System.out.println("Age : 26");
+	}
+
+}
